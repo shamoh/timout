cd9c4f785903341cb55d291b797d82549bb233eb
Libor Kramoliš <libor.kramolis@gmail.com>
2012-01-07 16:54:18 +0100

    Different sounds.

--- grep-versions.sh
+++ grep-versions.sh
@@ -1,10 +1,12 @@
 echo "******* Chrome WebStore version:"
 grep "version" src/manifest/manifest.json
-
+echo "*"
 echo "******* HTML5 AppCache version:"
 grep "Version" war/timout.appcache
-
+echo "*"
 echo "******* AppEngine version:"
 grep "version" war/WEB-INF/appengine-web.xml
-
+echo "-"
+echo "------- KONTROLA - index.html version:"
+grep "Version" war/index.html
 
