02a1b89023c2ce362c3723c3ea17c121ede8ea75
Libor Kramoliš <libor.kramolis@gmail.com>
2014-10-06 09:46:16 +0200

    Gradle ready.

--- grep-versions.sh
+++ grep-versions.sh
@@ -1,12 +1,12 @@
 echo "******* Chrome WebStore version:"
-grep "version" src/manifest/manifest.json
+grep "\"version" src/manifest/manifest.json
 echo "*"
 echo "******* HTML5 AppCache version:"
-grep "Version" war/timout.appcache
+grep "Version" src/main/webapp/timout.appcache
 echo "*"
 echo "******* AppEngine version:"
-grep "version" war/WEB-INF/appengine-web.xml
+grep "version" src/main/webapp/WEB-INF/appengine-web.xml
 echo "-"
 echo "------- KONTROLA - index.html version:"
-grep "Version" war/index.html
+grep "Version" src/main/webapp/index.html
 
--- war/WEB-INF/routes.groovy
+++
@@ -1,5 +0,0 @@
-
-get "/", forward: "/WEB-INF/pages/index.gtpl"
-get "/datetime", forward: "/datetime.groovy"
-
-//get "/favicon.ico", redirect: "/images/gaelyk-small-favicon.png"
